827b1fabf26099204378e4ae4d1897385fdcd978
kino6052 <kino6052@colorado.edu>
2015-01-10 18:32:23 -0700

    Finished uniqueGeneNumber

--- uniqueGeneSuperset/uniqueGeneSuperset.py
+++ uniqueGeneSuperset/uniqueGeneSuperset.py
@@ -17,17 +17,68 @@
 ## ALGORITHM: 
 ##
 
+from itertools import chain, combinations
+
 ## TO DO:
 ## - FUNCTION THAT READS INPUT AND MAKES A LIST OF GENOME FILES
-def input(): # (M) return list of genome files
+def input(fileLoc): # (M) return list of genome files
+    file = open(fileLoc, 'r')
+    return file
 ## - FUNCTION THAT CREATES A SUPERSET FROM THE ELEMENTS IN A LIST
-def superset(): # (M) return a list with all possible subsets
+def superset(list): # (M) return a list with all possible subsets
+    return chain.from_iterable(combinations(list,r)\
+                                for r in range(len(list)+1))
 ## - FUNCTION THAT CREATES A DICTIONARY WITH gene SETS (VALUE) FOR EVERY SET IN A SUPERSET (KEY)
-def supersetDict(): # (M) return a dictionary with gene sets for every organism set in a superset
+def geneSet(organism):
+    geneSet = set([])
+    for line in input('./input/genomes/' + str(organism)):
+        line = line.split()
+        if 'EJZ' in line[0]:
+            geneSet.add(line[0])
+        if 'EJZ' in line[1]:
+            geneSet.add(line[1])
+    return geneSet
+            
+
+def supersetDict(superset, dictionary): # (M) return a dictionary with gene sets for every organism set in a superset
+    supersetDict = {}
+    for s in superset:
+        supersetDict[s] = set([])
+        for element in s:
+            supersetDict[s] = supersetDict[s].union(dictionary[element])
+    return supersetDict
 ## - FUNCTION THAT CREATES A REFERENCE SET
-def referenceSet(): # (M) return a set of M. vaccae genes 
+def referenceSet(fileLoc): # (M) return a set of M. vaccae genes 
+    referenceSet = set([])
+    for line in input(fileLoc):
+        if 'EJZ' in line:
+            referenceSet.add(line[1:9])
+    return referenceSet
 ## - FUNCTION THAT OUPUTS THE AMOUNT OF UNIQUE GENES
-def uniqueGeneNumber(): # (M) return the number of genes
+def uniqueGeneNumber(supersetDict, referenceSet): # (M) return the number of genes
+    resultDict = {}
+    for set in supersetDict:
+        diffSet = referenceSet - supersetDict[set]
+        resultDict[set] = len(diffSet)
+    return resultDict
 ## - FUNCTION THAT CREATES THE OUTPUT FILE
-def output(): # (M) void
+def output(fileLoc): # (M) void
+    file = open(fileLoc, 'w')
+    return file
 ## - ERROR HANDLING
+
+def main():
+    testList = [1, 2, 3, 4, 5, 6, 7, 9]
+    supSet = superset(testList)
+    refSet = referenceSet('./input/mvaccae.fa')
+    genomeDict = {}
+    for i in testList:
+        genomeDict[i] = geneSet(i)
+    ssDict = supersetDict(supSet, genomeDict)
+    gNum = uniqueGeneNumber(ssDict, refSet)
+    result = output('./output/uniqueGeneSuperset.txt')
+    for i in gNum:
+        result.write(str(i) + '\t' + str(gNum[i]) + '\n')
+    result.close()
+    
+main()
